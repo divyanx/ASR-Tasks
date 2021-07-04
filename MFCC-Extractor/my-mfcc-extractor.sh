b55cd9ae19955beab5b789c23d819b501dc2f2f7
Divyansh <55907095+divyanx@users.noreply.github.com>
2021-07-05 04:34:26 +0530

    Change default to data directory

--- MFCC-Extractor/my-mfcc-extractor.sh
+++ MFCC-Extractor/my-mfcc-extractor.sh
@@ -4,17 +4,18 @@
 #The scripts when called with an audio file as argument extracts the MFCC features from it.
 . ./cmd.sh
 . ./path.sh
-
+mkdir -p data/convert_me
+mdata=./data/convert_me
 audio=$1
-touch MyData/wav.scp
-touch MyData/spk2utt
+touch $mdata/wav.scp
+touch $mdata/spk2utt
 audiofilename=$(basename $audio)
 audioindex=$(echo $audiofilename | sed 's/\./\ /g')
-echo "lbi-$audioindex -c -d -s ./$audio |" > MyData/wav.scp
+echo "lbi-$audioindex -c -d -s ./$audio |" > $mdata/wav.scp
 utterance=$(echo "$audiofilename" | cut -f 1 -d '.')
-echo "$utterance lbi-$utterance" > MyData/spk2utt
-echo "lbi-$utterance $utterance" > MyData/utt2spk
-steps/make_mfcc.sh --cmd "$train_cmd" --nj 1 MyData My_make_mfcc My_mfccdir
+echo "$utterance lbi-$utterance" > $mdata/spk2utt
+echo "lbi-$utterance $utterance" > $mdata/utt2spk
+steps/make_mfcc.sh --cmd "$train_cmd" --nj 1 $mdata My_make_mfcc My_mfccdir
 echo "---------------------------------------------------------------------------------------------"
 echo "MFCC features has been created in My_mfccdir in the folder containing this script"
 echo "Logs are in My_make_mfcc"
